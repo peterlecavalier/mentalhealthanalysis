946fca88510bac80b51eb69847c7f2ecdd698853
Peter LeCavalier <pele6150@colorado.edu>
2022-10-20 18:25:39 -0600

    Fixed issue with grouping

--- test_grouping.py
+++ test_grouping.py
@@ -2,7 +2,11 @@ from label_parsing import label_parser
 
 lp = label_parser()
 
-groups = lp.desc_groups()
-for i in groups:
+# List of lists of group DESCRIPTIONS
+desc_groups = lp.desc_groups()
+
+# List of lists of group VARIABLE NAMES
+var_groups = lp.var_groups()
+
+for i in desc_groups:
     print(i)
-#print(lp.desc_groups())
